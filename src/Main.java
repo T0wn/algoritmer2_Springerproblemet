6591a074acb6d947f45cc16de5328faf3e946616
T0wn <eirijomine@hotmail.com>
2019-02-27 21:56:59 +0100

    Algoritme ferdig. Mangler kommentarer.

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,41 @@
+import java.util.Scanner;
 import java.util.Stack;
 
 public class Main {
 
     public static Stack<Integer> trekkStatus = new Stack<Integer>();
 
-    public static void finnOgFlytt(Sjakkbrett brett, int nesteTrekk) {
+    public static void finnOgFlytt(Sjakkbrett brett, int nesteTrekk, boolean firstCall) {
         if ( brett.getTrekknr() <= brett.getSquaresInBoard() ) {
-            if (brett.getPossibleMoves().size() > nesteTrekk) {
+            if (trekkStatus.size() == 0 && !firstCall) {
+                System.out.println("Finnes ingen løsning");
+            }
+            else if (brett.getPossibleMoves().size() > nesteTrekk) {
                 brett.flyttBrikke( brett.getPossibleMoves().get(nesteTrekk).get(0), brett.getPossibleMoves().get(nesteTrekk).get(1) );
                 trekkStatus.push(nesteTrekk);
-//                System.out.println(trekkStatus);
-//                brett.printBrett();
-                finnOgFlytt(brett, 0);
+                finnOgFlytt(brett, 0, false);
             }
             else {
                 brett.flyttTilbake();
-//                brett.printBrett();
-                finnOgFlytt(brett, trekkStatus.pop() + 1);
+                finnOgFlytt(brett, trekkStatus.pop() + 1, false);
             }
         }
         else {
+//            printer løsningen
             brett.printBrett();
         }
     }
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Størrelse på sjakkbrett           : ");
+        int boardSize = scanner.nextInt();
+        System.out.print("Startposisjon springer (x-verdi)  : ");
+        int startX = scanner.nextInt();
+        System.out.print("Startposisjon springer (y-verdi)  : ");
+        int startY = scanner.nextInt();
 
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.print("Størrelse på sjakkbrett           : ");
-//        int boardSize = scanner.nextInt();
-//        System.out.print("Startposisjon springer (x-verdi)  : ");
-//        int startX = scanner.nextInt();
-//        System.out.print("Startposisjon springer (y-verdi)  : ");
-//        int startY = scanner.nextInt();
-
-//        Sjakkbrett brett = new Sjakkbrett(boardSize, startX, startY);
-        Sjakkbrett brett = new Sjakkbrett(5, 1, 1);
-        brett.printBrett();
-        finnOgFlytt(brett, 0);
-
+        Sjakkbrett brett = new Sjakkbrett(boardSize, startX, startY);
+        finnOgFlytt(brett, 0, true);
     }
-
 }
