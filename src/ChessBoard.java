cd3fb00cb38f8afb12369f7748d8f54ef0611a0b
T0wn <eirijomine@gmail.com>
2021-02-25 22:02:55 +0100

    General code improvements

---
+++ src/ChessBoard.java
@@ -0,0 +1,97 @@
+import java.util.ArrayList;
+
+public class ChessBoard {
+    private int[][] board;
+    private int moveNr = 1;
+    private int[] currentPos;
+    private static int[] xDelta = {1, 1, -1, -1, 2, 2, -2, -2};
+    private static int[] yDelta = {2, -2, 2, -2, 1, -1, 1, -1};
+
+    public ChessBoard(int size, int x, int y) {
+        board = new int[size][size];
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                board[i][j] = 0;
+            }
+        }
+
+        moveTo(x-1, y-1);
+    }
+
+    public void solve() {
+        if (moveNr > Math.pow(board.length, 2)) {
+            System.out.println("Solution:");
+            printBoard();
+            System.exit(0);
+        } else {
+
+            ArrayList<Integer> moves = possibleMoves();
+            for (int i = 0; i < moves.size(); i++) {
+                int newX = currentPos[0] + xDelta[moves.get(i)];
+                int newY = currentPos[1] + yDelta[moves.get(i)];
+
+                moveTo(newX, newY);
+                solve();
+            }
+            if (moveNr == 2) {
+                System.out.println("No solution!");
+                System.exit(0);
+            }
+
+            moveBack();
+
+        }
+    }
+
+    private void setCurrentPos(int x, int y) {
+        currentPos = new int[]{x, y};
+    }
+
+    public void moveTo(int x, int y) {
+        board[y][x] = moveNr;
+        moveNr++;
+        setCurrentPos(x, y);
+    }
+
+    public void moveBack() {
+        board[currentPos[1]][currentPos[0]] = 0;
+        moveNr -= 1;
+
+        for (int y = 0; y < board.length; y++) {
+            for (int x = 0; x < board.length; x++) {
+                if (board[y][x] == moveNr - 1) {
+                    setCurrentPos(x, y);
+                    break;
+                }
+            }
+        }
+    }
+
+    public ArrayList<Integer> possibleMoves() {
+        ArrayList<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < xDelta.length; i++) {
+            try {
+                if (board[currentPos[1] + yDelta[i]][currentPos[0] + xDelta[i]] == 0) {
+                    result.add(i);
+                }
+            } catch (Exception outOfBoundsException) {}
+        }
+
+        return result;
+    }
+
+    public void printBoard() {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                if (board[j][i] < 10) {
+                    System.out.print("0");
+                }
+                System.out.print(board[j][i] + " ");
+            }
+            System.out.print("\n");
+        }
+        System.out.print("\n");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,7 @@
 import java.util.Scanner;
-import java.util.Stack;
 
 public class Main {
-
-    private static Stack<Integer> trekkStatus = new Stack<Integer>();
-
-    private static void finnOgFlytt(Sjakkbrett brett, int nesteTrekk, boolean firstCall) {
-
-        // sjekker at problemet ikke er løst, altså om antall trekk tatt er mindre enn antall ruter på brettet.
-        if ( brett.getTrekknr() <= brett.getSquaresInBoard() ) {
-
-            // sjekker om det er flere mulige trekk videre som ikke er testet enda.
-            if (brett.getPossibleMoves().size() > nesteTrekk) {
-                brett.flyttBrikke( brett.getPossibleMoves().get(nesteTrekk).get(0), brett.getPossibleMoves().get(nesteTrekk).get(1) );
-                trekkStatus.push(nesteTrekk);
-                finnOgFlytt(brett, 0, false);
-            }
-            // Hvis det ikke er flere utesta trekk videre, så går man et trekk tilbake
-            else {
-                brett.flyttTilbake();
-                int nextMove = trekkStatus.pop();
-
-                // Hvis man har gått helt tilbake til start posisjonen så har man testet alle mulige trekk, og det er derfor
-                // ingen løsning.
-                if (trekkStatus.size() == 0) {
-                    System.out.println("Finnes ingen løsning");
-                }
-                else {
-                    finnOgFlytt(brett, nextMove + 1, false);
-                }
-            }
-        }
-        else {
-            // printer løsningen
-            brett.printBrett();
-        }
-
-    }
-
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
         System.out.print("Størrelse på sjakkbrett           : ");
         int boardSize = scanner.nextInt();
@@ -48,8 +10,8 @@ public class Main {
         System.out.print("Startposisjon springer (y-verdi)  : ");
         int startY = scanner.nextInt();
 
-        Sjakkbrett brett = new Sjakkbrett(boardSize, startX, startY);
-        finnOgFlytt(brett, 0, true);
-
+        ChessBoard board = new ChessBoard(boardSize, startX, startY);
+        board.printBoard();
+        board.solve();
     }
 }
